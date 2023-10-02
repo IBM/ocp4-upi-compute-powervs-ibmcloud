1c61faf16e1f34df1a193bc0b57b5760e8f22dbc
Paul Bastide <pbastide@us.ibm.com>
2023-10-02 14:02:17 -0400

    add support for multiple zones and multiple worker profiles across zones
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- ansible/post/files/destroy-workers.sh
+++ ansible/post/files/destroy-workers.sh
@@ -5,8 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 ################################################################
 
-# Var: ${self.triggers.count}
-COUNT="${1}"
+# Var: ${self.triggers.count_1}
+COUNT_1="${1}"
+
+# Var: ${self.triggers.count_2}
+COUNT_2="${2}"
+
+# Var: ${self.triggers.count_3}
+COUNT_3="${3}"
 
 # Var: self.triggers.name_prefix
 NAME_PREFIX="${2}"
@@ -14,7 +20,23 @@ NAME_PREFIX="${2}"
 IDX=0
 while [ "$IDX" -lt "$COUNT" ]
 do
-    echo "Removing the Worker: ${NAME_PREFIX}-worker-${IDX}"
+    echo "Removing the Worker Zone 1: ${NAME_PREFIX}-worker-1-${IDX}"
+    oc delete node ${NAME_PREFIX}-worker-${IDX} || true
+    IDX=$(($IDX + 1))
+done
+
+IDX=0
+while [ "$IDX" -lt "$COUNT_2" ]
+do
+    echo "Removing the Worker Zone 2: ${NAME_PREFIX}-worker-2-${IDX}"
+    oc delete node ${NAME_PREFIX}-worker-${IDX} || true
+    IDX=$(($IDX + 1))
+done
+
+IDX=0
+while [ "$IDX" -lt "$COUNT_3" ]
+do
+    echo "Removing the Worker Zone 3: ${NAME_PREFIX}-worker-3-${IDX}"
     oc delete node ${NAME_PREFIX}-worker-${IDX} || true
     IDX=$(($IDX + 1))
 done
--- ansible/post/files/remove-worker-taints.sh
+++ ansible/post/files/remove-worker-taints.sh
@@ -5,8 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 ################################################################
 
-# Var: ${self.triggers.count}
-COUNT="${2}"
+# Var: ${self.triggers.count_1}
+COUNT_1="${2}"
+
+# Var: ${self.triggers.count_2}
+COUNT_2="${3}"
+
+# Var: ${self.triggers.count_3}
+COUNT_3="${4}"
 
 # Var: self.triggers.name_prefix
 NAME_PREFIX="${1}"
@@ -14,7 +20,25 @@ NAME_PREFIX="${1}"
 IDX=0
 while [ "$IDX" -lt "$COUNT" ]
 do
-    echo "Removing the taint for Worker: ${NAME_PREFIX}-worker-${IDX}"
+    echo "Removing the taint for Worker: ${NAME_PREFIX}-worker-1-${IDX}"
+    oc adm taint node ${NAME_PREFIX}-worker-${IDX} node.cloudprovider.kubernetes.io/uninitialized- \
+        || true
+    IDX=$(($IDX + 1))
+done
+
+IDX=0
+while [ "$IDX" -lt "$COUNT" ]
+do
+    echo "Removing the taint for Worker: ${NAME_PREFIX}-worker-2-${IDX}"
+    oc adm taint node ${NAME_PREFIX}-worker-${IDX} node.cloudprovider.kubernetes.io/uninitialized- \
+        || true
+    IDX=$(($IDX + 1))
+done
+
+IDX=0
+while [ "$IDX" -lt "$COUNT" ]
+do
+    echo "Removing the taint for Worker: ${NAME_PREFIX}-worker-3-${IDX}"
     oc adm taint node ${NAME_PREFIX}-worker-${IDX} node.cloudprovider.kubernetes.io/uninitialized- \
         || true
     IDX=$(($IDX + 1))
