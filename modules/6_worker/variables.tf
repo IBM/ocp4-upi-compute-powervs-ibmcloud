f6d41bceb1266e28f46962cee45fdaefd6bf1601
Chandan Abhyankar <chandan.abhyankar@gmail.com>
2023-10-07 01:31:02 -0700

    Changes are added
    Signed-off-by: Chandan Abhyankar <chandan.abhyankar@gmail.com>

--- modules/6_worker/variables.tf
+++ modules/6_worker/variables.tf
@@ -5,27 +5,28 @@
 
 variable "name_prefix" {}
 variable "target_worker_sg_id" {}
-variable "rhcos_image_id" {}
+variable "rhcos_image_name" {}
+#variable "rhcos_image_id" {}
 variable "vpc_name" {}
 variable "vpc_key_id" {}
 variable "ignition_ip" {}
 
 
 variable "worker_1" {
-  type = object({ count = number, profile = string })
+  type = object({ count = number, profile = string, zone = string })
   default = {
     count   = 1
     profile = "cx2d-8x16"
-    zone    = ""
+    zone    = "ca-tor-1"
   }
   validation {
     condition     = lookup(var.worker_1, "count", 1) >= 1
-    error_message = "The worker.count value must be greater than 1."
+    error_message = "The worker_1.count value must be greater than 1."
   }
 }
 
 variable "worker_2" {
-  type = object({ count = number, profile = string })
+  type = object({ count = number, profile = string, zone = string })
   default = {
     count   = 0
     profile = "cx2d-8x16"
@@ -33,12 +34,12 @@ variable "worker_2" {
   }
   validation {
     condition     = lookup(var.worker_2, "count", 1) >= 0
-    error_message = "The worker.count value must be greater than 0."
+    error_message = "The worker_2.count value must be greater than 0."
   }
 }
 
 variable "worker_3" {
-  type = object({ count = number, profile = string })
+  type = object({ count = number, profile = string, zone = string })
   default = {
     count   = 0
     profile = "cx2d-8x16"
@@ -46,6 +47,6 @@ variable "worker_3" {
   }
   validation {
     condition     = lookup(var.worker_3, "count", 1) >= 0
-    error_message = "The worker.count value must be greater than 0."
+    error_message = "The worker_3.count value must be greater than 0."
   }
 }
--- modules/6_worker/worker.tf
+++ modules/6_worker/worker.tf
@@ -6,36 +6,54 @@
 # Creates an Intel VSI for the VPC and PowerVS workers 
 # Ref: https://github.com/openshift/installer/blob/master/data/data/powervs/cluster/dns/dns.tf
 
+data "ibm_is_image" "rhcos_image" {
+  count = 1
+  name  = var.rhcos_image_name
+}
+
 data "ibm_is_vpc" "vpc" {
   name = var.vpc_name
 }
 
 resource "ibm_is_instance" "workers_1" {
   count   = var.worker_1["count"]
-  name    = "${var.name_prefix}-worker-${count.index}"
+  name    = "ca-worker-test-1" #"${var.name_prefix}-worker-${count.index}" #"ca-worker-test-1"
   vpc     = data.ibm_is_vpc.vpc.id
-  zone    = var.worker_1["zone"]
+  zone    = var.worker_1["zone"] #"ca-tor-2"
   keys    = [var.vpc_key_id]
-  image   = var.rhcos_image_id
-  profile = var.worker_1["profile"] #
+  image   = data.ibm_is_image.rhcos_image[0].id #var.rhcos_image_id
+  profile = var.worker_1["profile"]             #
   # Profiles: https://cloud.ibm.com/docs/vpc?topic=vpc-profiles&interface=ui
   # "cx2d-8x16" - 8x16 includes 300G storage.
   resource_group = data.ibm_is_vpc.vpc.resource_group
 
   primary_network_interface {
-    subnet          = data.ibm_is_vpc.vpc.subnets[0].id
+    subnet          = "02r7-62686abe-3160-4be5-867f-9ceb914f4cac" #data.ibm_is_vpc.vpc.subnets[0].id
     security_groups = [var.target_worker_sg_id]
   }
 
-  user_data = base64encode(
-    templatefile(
-      "${path.cwd}/modules/6_worker/templates/worker.ign",
-      {
-        ignition_ip : var.ignition_ip,
-        name : base64encode("${var.name_prefix}-worker-1-${count.index}"),
-  }))
+  user_data = templatefile(
+    "${path.cwd}/modules/6_worker/templates/worker.ign",
+    {
+      ignition_ip : var.ignition_ip,
+      name : base64encode("ca-worker-test-1"),
+  })
 }
 
+# The PowerVS instance may take a few minutes to start (per the IPI work)
+resource "time_sleep" "wait_3_minutes" {
+  depends_on      = [ibm_is_instance.workers_1]
+  create_duration = "3m"
+}
+
+data "ibm_is_instance" "worker" {
+  count      = 1
+  depends_on = [time_sleep.wait_3_minutes]
+
+  name = ibm_is_instance.workers_1[count.index].name
+}
+
+/*
 resource "ibm_is_instance" "workers_2" {
   count   = var.worker_2["count"]
   name    = "${var.name_prefix}-worker-${count.index}"
@@ -87,3 +105,5 @@ resource "ibm_is_instance" "workers_3" {
         name : base64encode("${var.name_prefix}-worker-3-${count.index}"),
   }))
 }
+
+*/
