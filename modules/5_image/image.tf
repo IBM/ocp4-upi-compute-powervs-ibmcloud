19568000f38e47eaf71be17147f30276893c2e8f
Chandan Abhyankar <chandan.abhyankar@gmail.com>
2023-10-09 01:55:34 -0700

    Modifications to destroy node shell script
    Signed-off-by: Chandan Abhyankar <chandan.abhyankar@gmail.com>

--- modules/5_image/image.tf
+++ modules/5_image/image.tf
@@ -11,15 +11,15 @@ resource "ibm_resource_instance" "ibm_resource_instance" {
   name              = "${var.name_prefix}-bucket"
   resource_group_id = data.ibm_resource_group.resource_group.id
   service           = "cloud-object-storage"
-  plan              = "smart"
-  location          = var.vpc_region
+  plan              = "standard"
+  location          = "global" #var.vpc_region
 }
 
 resource "null_resource" "upload_rhcos_image" {
   depends_on = [ibm_resource_instance.ibm_resource_instance]
   connection {
     type        = "ssh"
-    user        = var.rhel_username
+    user        = "root" #var.rhel_username
     host        = var.bastion_public_ip
     private_key = file(var.private_key_file)
     agent       = var.ssh_agent
@@ -34,9 +34,9 @@ resource "null_resource" "upload_rhcos_image" {
   provisioner "remote-exec" {
     inline = [<<EOF
 echo 'Uploading rhcos image to ibmcloud'
-cd ocp4-upi-compute-powervs-ibmcloud/intel/image/upload_rhcos_image.sh
+cd ocp4-upi-compute-powervs-ibmcloud/intel/image
 chmod +x upload_rhcos_image.sh
-./upload_rhcos_image.sh "${var.ibmcloud_api_key}" "${var.vpc_region}" "${var.name_prefix}"
+./upload_rhcos_image.sh "${var.ibmcloud_api_key}" "${var.vpc_region}" "${var.resource_group}" "${var.name_prefix}"
 echo 'Done with rhcos image uploading to ibmcloud'
 EOF
     ]
