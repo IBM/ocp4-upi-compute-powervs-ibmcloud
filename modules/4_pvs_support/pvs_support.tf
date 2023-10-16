4f18ad42bd26d04166efd3115aaced234916a1ec
Paul Bastide <pbastide@us.ibm.com>
2023-10-16 12:42:29 -0400

    fix: remove TARGET_MTU 9100 references
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/4_pvs_support/pvs_support.tf
+++ modules/4_pvs_support/pvs_support.tf
@@ -95,29 +95,30 @@ EOF
   }
 }
 
-resource "null_resource" "adjust_mtu" {
-  depends_on = [null_resource.limit_csi_arch]
-  connection {
-    type        = "ssh"
-    user        = var.rhel_username
-    host        = var.bastion_public_ip
-    private_key = file(var.private_key_file)
-    agent       = var.ssh_agent
-    timeout     = "${var.connection_timeout}m"
-  }
-  provisioner "remote-exec" {
-    inline = [<<EOF
-oc patch Network.operator.openshift.io cluster --type=merge --patch \
-  '{"spec": { "migration": { "mtu": { "network": { "from": 1450, "to": 9000 } , "machine": { "to" : 9100} } } } }'
-EOF
-    ]
-  }
-}
+# Dev Note: adjust_mtu comes back if we need to modify the MTU
+# resource "null_resource" "adjust_mtu" {
+#   depends_on = [null_resource.limit_csi_arch]
+#   connection {
+#     type        = "ssh"
+#     user        = var.rhel_username
+#     host        = var.bastion_public_ip
+#     private_key = file(var.private_key_file)
+#     agent       = var.ssh_agent
+#     timeout     = "${var.connection_timeout}m"
+#   }
+#   provisioner "remote-exec" {
+#     inline = [<<EOF
+# oc patch Network.operator.openshift.io cluster --type=merge --patch \
+#   '{"spec": { "migration": { "mtu": { "network": { "from": 1450, "to": 9000 } , "machine": { "to" : 9100} } } } }'
+# EOF
+#     ]
+#   }
+# }
 
 # ovnkube between vpc/powervs requires routingViaHost for the LBs to work properly
 # ref: https://community.ibm.com/community/user/powerdeveloper/blogs/mick-tarsel/2023/01/26/routingviahost-with-ovnkuberenetes
 resource "null_resource" "set_routing_via_host" {
-  depends_on = [null_resource.adjust_mtu]
+  depends_on = [null_resource.limit_csi_arch]
   connection {
     type        = "ssh"
     user        = var.rhel_username
@@ -136,65 +137,66 @@ EOF
   }
 }
 
-resource "null_resource" "wait_on_mcp" {
-  depends_on = [null_resource.set_routing_via_host, null_resource.adjust_mtu]
-  connection {
-    type        = "ssh"
-    user        = var.rhel_username
-    host        = var.bastion_public_ip
-    private_key = file(var.private_key_file)
-    agent       = var.ssh_agent
-    timeout     = "${var.connection_timeout}m"
-  }
+# Dev Note: adjust_mtu comes back if we need to modify the MTU
+# resource "null_resource" "wait_on_mcp" {
+#   depends_on = [null_resource.set_routing_via_host, null_resource.adjust_mtu]
+#   connection {
+#     type        = "ssh"
+#     user        = var.rhel_username
+#     host        = var.bastion_public_ip
+#     private_key = file(var.private_key_file)
+#     agent       = var.ssh_agent
+#     timeout     = "${var.connection_timeout}m"
+#   }
 
-  # Dev Note: added hardening to the MTU wait, we wait for the condition and then fail
-  provisioner "remote-exec" {
-    inline = [<<EOF
-echo "-diagnostics-"
-oc get network cluster -o yaml | grep -i mtu
-oc get mcp
+#   # Dev Note: added hardening to the MTU wait, we wait for the condition and then fail
+#   provisioner "remote-exec" {
+#     inline = [<<EOF
+# echo "-diagnostics-"
+# oc get network cluster -o yaml | grep -i mtu
+# oc get mcp
 
-if [ $(oc get network.operator cluster -ojson | jq -r '.status.conditions[] | select(.type == "Degraded")' | grep message | grep 'invalid Migration.MTU.Network.From' | wc -l) -ne 0 ]
-then
-  echo "Check the Cluster's MTU, something is wrong"
-  exit 1
-fi
+# if [ $(oc get network.operator cluster -ojson | jq -r '.status.conditions[] | select(.type == "Degraded")' | grep message | grep 'invalid Migration.MTU.Network.From' | wc -l) -ne 0 ]
+# then
+#   echo "Check the Cluster's MTU, something is wrong"
+#   exit 1
+# fi
 
-echo 'verifying worker mc'
-start_counter=0
-timeout_counter=10
-mtu_output=`oc get mc 00-worker -o yaml | grep TARGET_MTU=9100`
-echo "(DEBUG) MTU FOUND?: $${mtu_output}"
-# While loop waits for TARGET_MTU=9100 till timeout has not reached 
-while [[ "$(oc get network cluster -o yaml | grep 'to: 9100' | awk '{print $NF}')" != "9100" ]]
-do
-  echo "waiting on worker"
-  sleep 30
-done
+# echo 'verifying worker mc'
+# start_counter=0
+# timeout_counter=10
+# mtu_output=`oc get mc 00-worker -o yaml | grep TARGET_MTU=9100`
+# echo "(DEBUG) MTU FOUND?: $${mtu_output}"
+# # While loop waits for TARGET_MTU=9100 till timeout has not reached 
+# while [[ "$(oc get network cluster -o yaml | grep 'to: 9100' | awk '{print $NF}')" != "9100" ]]
+# do
+#   echo "waiting on worker"
+#   sleep 30
+# done
 
-RENDERED_CONFIG=$(oc get mcp/worker -o json | jq -r '.spec.configuration.name')
-CHECK_CONFIG=$(oc get mc $${RENDERED_CONFIG} -ojson 2>&1 | grep TARGET_MTU=9100)
-while [ -z "$${CHECK_CONFIG}" ]
-do
-  echo "waiting on worker"
-  sleep 30
-  RENDERED_CONFIG=$(oc get mcp/worker -o json | jq -r '.spec.configuration.name')
-  CHECK_CONFIG=$(oc get mc $${RENDERED_CONFIG} -ojson 2>&1 | grep TARGET_MTU=9100)
-done
+# RENDERED_CONFIG=$(oc get mcp/worker -o json | jq -r '.spec.configuration.name')
+# CHECK_CONFIG=$(oc get mc $${RENDERED_CONFIG} -ojson 2>&1 | grep TARGET_MTU=9100)
+# while [ -z "$${CHECK_CONFIG}" ]
+# do
+#   echo "waiting on worker"
+#   sleep 30
+#   RENDERED_CONFIG=$(oc get mcp/worker -o json | jq -r '.spec.configuration.name')
+#   CHECK_CONFIG=$(oc get mc $${RENDERED_CONFIG} -ojson 2>&1 | grep TARGET_MTU=9100)
+# done
 
-# Waiting on output
-oc wait mcp/worker \
-  --for condition=updated \
-  --timeout=5m || true
+# # Waiting on output
+# oc wait mcp/worker \
+#   --for condition=updated \
+#   --timeout=5m || true
 
-echo '-checking mtu-'
-oc get network cluster -o yaml | grep 'to: 9100' | awk '{print $NF}'
-[[ "$(oc get network cluster -o yaml | grep 'to: 9100' | awk '{print $NF}')" == "9100" ]] || false
-echo "success on wait on mtu change"
-EOF
-    ]
-  }
-}
+# echo '-checking mtu-'
+# oc get network cluster -o yaml | grep 'to: 9100' | awk '{print $NF}'
+# [[ "$(oc get network cluster -o yaml | grep 'to: 9100' | awk '{print $NF}')" == "9100" ]] || false
+# echo "success on wait on mtu change"
+# EOF
+#     ]
+#   }
+# }
 
 # Dev Note: do this as the last step so we get a good worker ignition file downloaded.
 resource "null_resource" "latest_ignition" {
