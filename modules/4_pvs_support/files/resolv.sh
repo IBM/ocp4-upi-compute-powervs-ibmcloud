05679f234a05e7d79bd31c9c442ecac90f7e0701
Paul Bastide <pbastide@us.ibm.com>
2023-10-20 15:54:51 -0400

    fix: resolv butane adds a check before applying again
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/4_pvs_support/files/resolv.sh
+++ modules/4_pvs_support/files/resolv.sh
@@ -5,28 +5,34 @@
 
 # The script creates the butane configuration and then uses the configuration to populate the workers with the right resolv.conf file.
 
-dnf install -y butane
+VAL=$(oc get mc -o yaml | grep -c 99-worker-dnsconfig)
+if [ ${VAL} -ne 0 ]
+then 
+    dnf install -y butane
 
-cat << EOF > butane-resolv-conf.bu
-variant: openshift
-version: 4.12.0
-metadata:
-  name: 99-worker-dnsconfig
-  labels:
-    machineconfiguration.openshift.io/role: worker
-storage:
-  files:
-    - path: /etc/resolv.conf
-      mode: 0644
-      overwrite: true
-      contents:
-        inline: |
-          search $(hostname --long)
-          nameserver $(hostname -i | awk '{print $NF}')
-EOF
+    cat << EOF > butane-resolv-conf.bu
+    variant: openshift
+    version: 4.12.0
+    metadata:
+    name: 99-worker-dnsconfig
+    labels:
+        machineconfiguration.openshift.io/role: worker
+    storage:
+    files:
+        - path: /etc/resolv.conf
+        mode: 0644
+        overwrite: true
+        contents:
+            inline: |
+            search $(hostname --long)
+            nameserver $(hostname -i | awk '{print $NF}')
+    EOF
 
-butane butane-resolv-conf.bu > 99-worker-dnsconfig.yaml
-cat 99-worker-dnsconfig.yaml
+    butane butane-resolv-conf.bu > 99-worker-dnsconfig.yaml
+    cat 99-worker-dnsconfig.yaml
 
-oc apply -f 99-worker-dnsconfig.yaml
-echo "Done creating the dnsconfig for the workers"
+    oc apply -f 99-worker-dnsconfig.yaml
+    echo "Done creating the dnsconfig for the workers"
+else 
+    echo "Skipping the butane changes"
+fi
