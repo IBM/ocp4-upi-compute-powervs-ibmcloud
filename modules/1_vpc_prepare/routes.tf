637ee870ad327d0d3e2c065cfa9780c70fb6533d
Paul Bastide <pbastide@us.ibm.com>
2023-10-23 11:06:36 -0400

    fix: add idempotency for vpc route add
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/1_vpc_prepare/routes.tf
+++ modules/1_vpc_prepare/routes.tf
@@ -3,7 +3,17 @@
 # SPDX-License-Identifier: Apache-2.0
 ################################################################
 
+data "ibm_is_vpc_routing_tables" "rts" {
+  vpc = data.ibm_is_vpc.vpc.id
+}
+
+locals {
+  rt_matches = [for rt in data.ibm_is_vpc_routing_tables.rts.routing_tables : rt if endswith(rt.name, "to-powervs-route-1")]
+  rt_exists  = length(local.sg_matches) > 0 ? 0 : 1
+}
+
 resource "ibm_is_vpc_routing_table_route" "route_to_powervs" {
+  count         = local.rt_exists
   vpc           = data.ibm_is_vpc.vpc.id
   routing_table = data.ibm_is_vpc.vpc.default_routing_table
   zone          = data.ibm_is_vpc.vpc.subnets[0].zone
