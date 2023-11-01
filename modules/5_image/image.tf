40f9812ee22c44af69d07ae3a319fc9352408ee4
Paul Bastide <pbastide@us.ibm.com>
2023-11-01 15:59:23 -0400

    OCTOPUS-527: changed to allow_cleanup
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/5_image/image.tf
+++ modules/5_image/image.tf
@@ -14,6 +14,7 @@ data "ibm_resource_group" "resource_group" {
 resource "ibm_resource_instance" "cos_instance" {
   name              = "${var.name_prefix}-mac-intel-cos"
   resource_group_id = data.ibm_resource_group.resource_group.id
+  allow_cleanup     = true
   service           = "cloud-object-storage"
   plan              = "standard"
   location          = "global"
