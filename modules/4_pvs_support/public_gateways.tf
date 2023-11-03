b96e24d06080252290f9721e2f0cbd9e13464daa
Paul Bastide <pbastide@us.ibm.com>
2023-11-02 20:41:35 -0400

    comment out the public gateways feature
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/4_pvs_support/public_gateways.tf
+++ modules/4_pvs_support/public_gateways.tf
@@ -19,22 +19,22 @@
 #     ibmcloud is public-gateway-create my-public-gateway my-vpc us-south-1
 
 
-data "ibm_is_subnets" "sn" {
-  routing_table_name = data.ibm_is_vpc.vpc.default_routing_table_name
-}
+# data "ibm_is_subnets" "sn" {
+#   routing_table_name = data.ibm_is_vpc.vpc.default_routing_table_name
+# }
 
-locals {
+# locals {
 
-}
+# }
 
-resource "ibm_is_public_gateway" "example" {
-  count = length(data.ibm_is_subnets.subnets.subnets)
-  name  = data.ibm_is_subnets.sn.subnets[count.index]
-  vpc   = ibm_is_vpc.example.id
-  zone  = "eu-gb-1"
-}
+# resource "ibm_is_public_gateway" "example" {
+#   count = length(data.ibm_is_subnets.subnets.subnets)
+#   name  = data.ibm_is_subnets.sn.subnets[count.index]
+#   vpc   = ibm_is_vpc.example.id
+#   zone  = "eu-gb-1"
+# }
 
-resource "ibm_is_subnet_public_gateway_attachment" "example" {
-  subnet         = ibm_is_subnet.example.id
-  public_gateway = ibm_is_public_gateway.example.id
-}
+# resource "ibm_is_subnet_public_gateway_attachment" "example" {
+#   subnet         = ibm_is_subnet.example.id
+#   public_gateway = ibm_is_public_gateway.example.id
+# }
