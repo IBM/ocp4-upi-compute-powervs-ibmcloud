6a8a39c70f14cb135b648ce4352b2ebf798fcc66
Paul Bastide <pbastide@us.ibm.com>
2023-11-08 13:21:02 -0500

    OCTOPUS-534: subnets not propogated to the pvs_prepare where the routes are added
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/1_vpc_prepare/outputs.tf
+++ modules/1_vpc_prepare/outputs.tf
@@ -20,5 +20,5 @@ output "target_worker_sg_id" {
 }
 
 output "mac_vpc_subnets" {
-  value = data.ibm_is_subnets.vpc_subnets.subnets[*].ipv4_cidr_block
+  value = var.create_custom_subnet ? [ibm_is_subnet.subnet_worker_zone_1[0].ipv4_cidr_block, ibm_is_subnet.subnet_worker_zone_2[0].ipv4_cidr_block, ibm_is_subnet.subnet_worker_zone_3[0].ipv4_cidr_block] : data.ibm_is_subnets.vpc_subnets.subnets[*].ipv4_cidr_block
 }
