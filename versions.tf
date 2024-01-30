3b2da74b1a9078550c64c72805f332d185da010e
Paul Bastide <pbastide@us.ibm.com>
2024-01-30 13:24:47 -0500

    chore: upgrade IBM-Cloud/ibm to v1.62.0
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- versions.tf
+++ versions.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "~> 1.61.0"
+      version = "~> 1.62.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -24,4 +24,5 @@ terraform {
   }
   required_version = ">= 1.5.0"
 }
+
 
