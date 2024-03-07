b90c59c552944ba8cbe82c8aae08cb930527d7a6
Paul Bastide <pbastide@us.ibm.com>
2024-03-07 10:35:15 -0500

    ci: bump v1.62 to v1.63
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- versions.tf
+++ versions.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "~> 1.62.0"
+      version = "~> 1.63.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -24,5 +24,6 @@ terraform {
   }
   required_version = ">= 1.5.0"
 }
+
 
 
